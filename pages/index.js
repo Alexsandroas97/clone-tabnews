21e46a1f8d65702c0d377883b1ae1cf06bf4cd04
Alexsandroas97 <alexsandroas97@outlook.com>
2025-01-20 10:10:53 +0000

    adiciona ´.prettierignore´

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 function Home() {
   return (
     <h1>
-      Deus Te Ama e Eu tambem! Minha Princesa! Se você me ama, dá uma risadinha!{" "}
+      Deus Te Ama e Eu tambem! Minha Princesa! Se você me ama, dá uma risadinha!
     </h1>
   );
-}
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
 }
 
 export default Home;
